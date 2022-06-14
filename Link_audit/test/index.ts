22d36b9c68a8d136e2dab096c00413ecb81905c5
dnsdudrla97 <dnsdudrla97@naver.com>
2022-06-14 13:28:01 +0900

    mod: ch3

--- Link_audit/test/index.ts
+++ Link_audit/test/index.ts
@@ -1,7 +1,27 @@
-import { expect } from "chai";
+import { expect, use, util } from "chai";
 import { ethers } from "hardhat";
+import { BigNumber, Contract, Signer } from "ethers";
+import { MockProvider, deployContract, solidity } from "ethereum-waffle";
+import bnJs from "bn.js";
+import chai from "chai";
 
-describe("Greeter", function () {
+// Enable and inject BN dependency
+chai.use(require("chai-bn")(bnJs));
+use(solidity);
+let deployer: Signer;
+let user: Signer;
+
+// contract address
+let linkVault: Contract;
+let linkToken: Contract;
+
+// user data logic
+describe("Greeter", () => {
+  beforeEach(async () => {
+    [deployer] = await ethers.getSigners();
+
+  });
+
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
